7436ab0bafb03c91ca44d4607a3a0ad263bc5517
Seokho Youn <axlover@hotmail.com>
2019-01-21 20:38:27 +0900

    putData() 메소드 추가
    addSchedule 메소드와 modifySchedule 메소드 코드의 중복해결

--- src/sche/controller/ScheduleFC.java
+++ src/sche/controller/ScheduleFC.java
@@ -13,9 +13,7 @@ public class ScheduleFC implements ISchedule{
 	HashMap<String,Schedule> hm = new HashMap<>();
 	ArrayList<String> keys = new ArrayList<String>();
 	
-	@Override
-	public void addSchedule(String index) {
-		
+	public Schedule putData() {
 		System.out.println("일정 제목 입력 : ");
 		String title = sc.nextLine();
 		System.out.println("일정 시간 입력 : ");
@@ -26,40 +24,49 @@ public class ScheduleFC implements ISchedule{
 		while(!(line = sc.nextLine()).equals("exit")) {
 			sb.append(line);
 		}
+		return new Schedule(title,sb.toString(),time);
+	}
+	
+	@Override
+	public void addSchedule(String index) {
+		
+		Schedule sd = putData();
 		for(int i = 1; i < 100; i++) {
 			if(!(hm.containsKey(index+String.format("%02d", i)))) {
-				hm.put(index+String.format("%02d", i), new Schedule(title, sb.toString(),time));
+				hm.put(index+String.format("%02d", i), sd);
 				keys.add(index+String.format("%02d", i));
 				break;
 			}
 		}
-		
 	}
 
 	@Override
 	public void modifySchedule(String index) {
-		keys.sort(new AscDate());
-		for(Iterator<String> it2 = keys.iterator(); it2.hasNext();) {
-			String key = it2.next();
-			if(key.substring(0, 8).contains(index)) {
-			System.out.println("Sche. NO : " + key);
-			System.out.println("TO DO : "+hm.get(key).getTitle());
-			System.out.println("TIME : "+hm.get(key).getTime());
-			System.out.println("COMMENT : "+hm.get(key).getText()+"\n");
-			}
-		}
+		printSchedule(index);
 		System.out.println("변경 할 일정 번호 입력 : ");
 		String mnum = sc.next();
-		if(hm.containsKey(mnum)) 
-			addSchedule(index);
+		sc.nextLine();
+		if(hm.containsKey(mnum)) {
+			Schedule sd = putData();
+			hm.put(mnum, sd);
+		}
 		else
-			System.out.println("해당 일정 번호는 없습니다.");
+			System.out.println("해당 번호는 없습니다.");
 		
 	}
 
 	@Override
 	public void delSchedule(String index) {
-		
+		printSchedule(index);
+		System.out.println("삭제 할 일정 번호 입력 : ");
+		String mnum = sc.next();
+		if(hm.containsKey(mnum)) {
+			hm.remove(mnum);
+			keys.remove(mnum);
+			System.out.println("삭제가 완료되었습니다.");
+		}
+		else
+			System.out.println("해당 번호는 없습니다.");
 	}
 
 	public void printSchedule() {
@@ -74,6 +81,18 @@ public class ScheduleFC implements ISchedule{
 		
 	}
 	
+	public void printSchedule(String index) {
+		keys.sort(new AscDate());
+		for(Iterator<String> it2 = keys.iterator(); it2.hasNext();) {
+			String key = it2.next();
+			if(key.substring(0, 8).contains(index)) {
+			System.out.println("Sche. NO : " + key);
+			System.out.println("TO DO : "+hm.get(key).getTitle());
+			System.out.println("TIME : "+hm.get(key).getTime());
+			System.out.println("COMMENT : "+hm.get(key).getText()+"\n");
+			}
+		}
+	}
 	
 	
 
